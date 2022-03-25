d2d0f3aafa93436f8e1cd188220555e482f5e9d8
wityat <wityat2013@yandex.ru>
2022-03-25 04:54:02 +0300

    NEW RECOGNIZE SYSTEM + ADDED CHOOSE FOUNDED INGREDIENTS

--- app/src/main/java/com/example/receiptsbyphoto/RecognizeActivity.kt
+++ app/src/main/java/com/example/receiptsbyphoto/RecognizeActivity.kt
@@ -26,6 +26,22 @@ import com.spoonacular.client.model.InlineResponse2001
 import kotlinx.coroutines.*
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.widget.Toast
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.ObjectDetection
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.default
+import id.zelory.compressor.constraint.destination
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import java.io.IOException
 
 
 class MyApplication : Application() {
@@ -64,31 +80,66 @@ class RecognizeActivity : AppCompatActivity(){
 
         GlobalScope.launch(Dispatchers.Main) {
             progressBar.visibility = ProgressBar.VISIBLE
-            val jodForGettingIngredients = async { getIngredients(bitmap) }
+            val jodForGettingIngredients = async { getIngredients(photoPath) }
             ingredients = jodForGettingIngredients.await()
+            val checkedIngredients = booleanArrayOf()
+            ingredients.forEach{ _ -> checkedIngredients.toMutableList().add(false)}
             Log.d("ingredients", ingredients.toString())
-            textView.text = "Getting recipes list..."
-            val jodForGettingRecipes = async { getRecipes(ingredients) }
-            val app = applicationContext as MyApplication
-            app.recipes = jodForGettingRecipes.await()
-            progressBar.visibility = ProgressBar.INVISIBLE
-            Log.d("recipes size", app.recipes.size.toString())
-            if (app.recipes.isNotEmpty()){
-                launch {startActivity(Intent(app, RecipeActivity::class.java))}
-                finish()
+
+            suspend fun contin(choosedIngredients: List<String>) {
+                textView.text = "Getting recipes list..."
+                val jodForGettingRecipes = async { getRecipes(choosedIngredients) }
+                val app = applicationContext as MyApplication
+                app.recipes = jodForGettingRecipes.await()
+                progressBar.visibility = ProgressBar.INVISIBLE
+                Log.d("recipes size", app.recipes.size.toString())
+                if (app.recipes.isNotEmpty()) {
+                    launch { startActivity(Intent(app, RecipeActivity::class.java)) }
+                    finish()
+                } else {
+                    val builder = AlertDialog.Builder(imageView.context)
+                    builder.setTitle("Unable to recognize")
+                    builder.setMessage("Cannot recognize food ingredients on your picture. Try take another picture, please.")
+                    Log.d("!recipes size", app.recipes.size.toString())
+                    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+                        finish()
+                    }
+                    Log.d("!!!recipes size", app.recipes.size.toString())
+                    builder.setPositiveButton(
+                        "OK",
+                        DialogInterface.OnClickListener(function = positiveButtonClick)
+                    )
+                    builder.show()
+                }
             }
-            else{
-                val builder = AlertDialog.Builder(imageView.context)
-                builder.setTitle("Unable to recognize")
-                builder.setMessage("Cannot recognize food ingredients on your picture. Try take another picture, please.")
-                Log.d("!recipes size", app.recipes.size.toString())
-                val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+
+            val builder1 = AlertDialog.Builder(imageView.context)
+            builder1.setTitle("Выберите ингредиенты")
+                .setMultiChoiceItems(ingredients.toTypedArray(), checkedIngredients) {
+                        dialog, which, isChecked ->
+                    checkedIngredients[which] = isChecked
+                    val name = ingredients[which] // Get the clicked item
+                    Toast.makeText(imageView.context, name, Toast.LENGTH_LONG).show()
+                }
+                .setPositiveButton("Готово"
+                ) {
+                        dialog, id ->
+                    val choosedIngredients = listOf<String>()
+                        for (i in ingredients.indices) {
+                        val checked = checkedIngredients[i]
+                        if (checked) {
+                            Log.i("Dialog", ingredients[i])
+                            choosedIngredients.toMutableList().add(ingredients[i])
+                        }
+
+                    }
+                    launch {contin(choosedIngredients)}
+                }
+                .setNegativeButton("Отмена") {
+                        dialog, _ ->  dialog.cancel()
                     finish()
                 }
-                Log.d("!!!recipes size", app.recipes.size.toString())
-                builder.setPositiveButton("OK", DialogInterface.OnClickListener(function =positiveButtonClick))
-                builder.show()
-            }
+            builder1.show()
 
 
         }
@@ -116,33 +167,139 @@ class RecognizeActivity : AppCompatActivity(){
 //        return result
 //    }
 
-    private fun getIngredients(image: Bitmap): List<String> {
-        ImageAnnotatorClient.create(ImageAnnotatorSettings.newBuilder().setCredentialsProvider(
-            FixedCredentialsProvider.create(ServiceAccountCredentials
-                .fromStream(getAssets().open("food-recognize.json")))).build()).use { vision ->
+    private fun getIngredients(photoPath: String?): List<String> {
+        data class Respons(
+            val ingredient: Map<String, Float>,
+        )
 
-            val imgBytes: ByteString = ByteString.copyFrom(encodeImage(image))
+        var res: Respons
+        val moshi: Moshi = Moshi.Builder().build()
+        val jsonAdapter: JsonAdapter<Respons> = moshi.adapter(Respons::class.java)
 
-            // Builds the image annotation request
-            val requests: MutableList<AnnotateImageRequest> =
-                ArrayList<AnnotateImageRequest>()
-            val img: Image = Image.newBuilder().setContent(imgBytes).build()
-            val feat: Feature =
-                Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build()
-            val request: AnnotateImageRequest =
-                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build()
-            requests.add(request)
 
-            // Performs label detection on the image file
-            val response: BatchAnnotateImagesResponse = vision.batchAnnotateImages(requests)
-            val responses: List<AnnotateImageResponse> = response.responsesList
+        val client = OkHttpClient()
+        val context = this
 
-            return if (responses.isNotEmpty())
-                responses[0].labelAnnotationsList.map { it.description }
-//                responses[0].localizedObjectAnnotationsList.map { it.name }
-            else
-                listOf("nothing")
+        fun compres() = runBlocking {
+            val compressedImageFile = Compressor.compress(context, File(photoPath.toString())) {
+                default()
+                destination(File(photoPath.toString()))
+            }
+            return@runBlocking compressedImageFile
         }
+        // Use the imgur image upload API as documented at https://api.imgur.com/endpoints/image
+        val requestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+//                .addFormDataPart("title", "Square Logo")
+            .addFormDataPart("image", "image.jpg",
+                compres().asRequestBody("image/jpeg".toMediaType()))
+            .build()
+
+        val request = Request.Builder()
+            .header("token", "kreks")
+            .url("https://9141-109-184-53-6.ngrok.io/image/get_ingredients/")
+            .post(requestBody)
+            .build()
+
+        try{
+            client.newCall(request).execute().use { response ->
+                if (response.isSuccessful) {
+                    res = jsonAdapter.fromJson(response.body!!.source())!!
+                } else {
+                    throw IOException("Unexpected code $response")
+//                    res = Respons(mapOf())
+                }
+            }
+        }catch (e: Exception){
+//                val builder = AlertDialog.Builder(imageView.context)
+//                builder.setTitle("Server is temporary unavailable")
+//                builder.setMessage("Server is temporary unavailable. Try again later, please.")
+//                val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+//                    finish()
+//                }
+//                builder.setPositiveButton(
+//                    "OK",
+//                    DialogInterface.OnClickListener(function = positiveButtonClick)
+//                )
+//                builder.show()
+            Log.d("ERROR", e.toString())
+                res = Respons(mapOf())
+            }
+
+
+        if (res.ingredient.isNotEmpty())
+            return res.ingredient.map{ it.key }
+        else
+            return listOf()
+
+//
+//        val options = ObjectDetectorOptions.Builder()
+//            .setDetectorMode(ObjectDetectorOptions.SINGLE_IMAGE_MODE)
+//            .enableMultipleObjects()
+//            .enableClassification()  // Optional
+//            .build()
+//        val objectDetector = ObjectDetection.getClient(options)
+//
+//        val image = InputImage.fromBitmap(image, 0)
+//        var ingredients = listOf<String>()
+//
+//        objectDetector.process(image)
+//            .addOnSuccessListener { detectedObjects ->
+//                // Task completed successfully
+//                // ...
+//                for (detectedObject in detectedObjects) {
+//                    for (label in detectedObject.labels) {
+//                        val text = label.text
+//                        val confidence = label.confidence
+//                        val index = label.index
+//                        Log.d("DETECTED", "$text $confidence $index")
+//                        ingredients.toMutableList().add(text)
+//                    }
+//
+//                    ingredients = ingredients + detectedObject.labels.map { it.text }
+//
+//                }
+//                if (detectedObjects.isNotEmpty())
+//                    Log.d("SUCCESS", detectedObjects[0].labels[0].toString())
+//                else
+//                    Log.d("SUCCESS BUT NO OBJECTS", "NO OBJECTS DETECTED")
+//
+//            }
+//            .addOnFailureListener { e ->
+//                // Task failed with an exception
+//                // ...
+//                Log.d("ERROR!!!", e.toString())
+//                ingredients = listOf("nothing")
+//            }
+//        return ingredients
+//
+//
+//        ImageAnnotatorClient.create(ImageAnnotatorSettings.newBuilder().setCredentialsProvider(
+//            FixedCredentialsProvider.create(ServiceAccountCredentials
+//                .fromStream(getAssets().open("food-recognize.json")))).build()).use { vision ->
+//
+//            val imgBytes: ByteString = ByteString.copyFrom(encodeImage(image))
+//
+//            // Builds the image annotation request
+//            val requests: MutableList<AnnotateImageRequest> =
+//                ArrayList<AnnotateImageRequest>()
+//            val img: Image = Image.newBuilder().setContent(imgBytes).build()
+//            val feat: Feature =
+//                Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build()
+//            val request: AnnotateImageRequest =
+//                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build()
+//            requests.add(request)
+//
+//            // Performs label detection on the image file
+//            val response: BatchAnnotateImagesResponse = vision.batchAnnotateImages(requests)
+//            val responses: List<AnnotateImageResponse> = response.responsesList
+//
+//            return if (responses.isNotEmpty())
+//                responses[0].labelAnnotationsList.map { it.description }
+////                responses[0].localizedObjectAnnotationsList.map { it.name }
+//            else
+//                listOf("nothing")
+//        }
     }
 
     private fun encodeImage(bm: Bitmap): ByteArray {
